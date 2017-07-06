168b060061a692889b8ad3c69184184943573767
zhoujin <jin100chow@hotamil.com>
2017-07-06 23:02:29 +0800

    add order model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,8 @@ class CartsController < ApplicationController
      flash[:warning] = "You have been clean the cart"
      redirect_to carts_path
    end
+
+   def checkout
+     @order = Order.new
+   end
 end

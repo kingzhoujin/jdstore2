6537fa7e53cea852bbfac814d0454136d0e84832
zhoujin <jin100chow@hotamil.com>
2017-07-06 20:40:00 +0800

    clean carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,7 @@
 class CartsController < ApplicationController
+   def clean
+     current_cart.clean!
+     flash[:warning] = "You have been clean the cart"
+     redirect_to carts_path
+   end
 end
